1fe0661d1a1a280fa34cd0d37bec9e4e10d2cebf
jann88 <jannylynemiaz@gmail.com>
2023-06-15 15:38:11 -0700

    c program that prints text on a new line with puts keyword

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
-#!/bin/bash 
 #include <stdio.h>
+/**
+ * main - a c program that prints a text with the 'puts function' keyword 
+ *
+ * return should be 0 (success)
+ */
 
-int main()
+int main(void)
 {
-	char myStr = 'Programming is like building a multilingual puzzle';
+	char myStr[] = 'Programming is like building a multilingual puzzle';
+
 	puts(mySrt\n);
+
 	return (0);
 }
