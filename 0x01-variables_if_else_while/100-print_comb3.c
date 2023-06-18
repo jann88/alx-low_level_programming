b2ba7c0e9887cef3f19d3a8f9a4dabf00bb134c8
jann88 <jannylynemiaz@gmail.com>
2023-06-18 07:40:20 -0700

    prints only the smallest possible combinations of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry level
+ * prints all possible different combinations of 2 digits
+ * use putchar function
+ * the two digits must be different
+ * print only the smallest combinations of 2 digits
+ * do not use any variables of type char
+ * Numbers should be printed in ascending order, with two digits
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	int j = '0';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j) || (i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	return (0);
+}
