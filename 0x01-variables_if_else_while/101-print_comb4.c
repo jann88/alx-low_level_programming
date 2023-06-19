be14dec42f52d838f8a13df49c75aca78f1c8c6f
jann88 <jannylynemiaz@gmail.com>
2023-06-19 13:44:07 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,31 +8,30 @@
  */
 int main(void)
 {
-	int i;
+	int i, j, l;
 
-	int j;
-
-	int k;
-
-	for (i = '0'; i < '9'; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = i + '1'; j < '9'; j++)
+		for (j = 49; j < 58; j++)
 		{
-			for (k = j + '1'; k < '9'; k++)
+			for (k = 50; k < 58; k++)
 			{
-				if (i != j && j != k)
+				if (k > j && j > i)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-				}
-				if (i == '7' && j == '8' && k == '9')
-				{
-					putchar(',');
-					putchar(' ');
+					if (i != 55 || j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+
+					}
 				}
 			}
+
 		}
+
 	}
 	putchar('\n');
 	return (0);
