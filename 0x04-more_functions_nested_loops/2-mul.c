999ae225d074caea51635f8090247638299793d8
jann88 <jannylynemiaz@gmail.com>
2023-06-22 13:34:36 -0700

    multiplies 2 integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,9 @@
  * mul - multiplies two digits
  * @a: first integer to be tested
  * @b: second integer to be tested
- * @result: total after multiplication
- * Return: 0 
+ * Return: 0
  */
 int mul(int a, int b)
 {
-	int result = a * b;
-	return (result);
+	return (a * b);
 }
