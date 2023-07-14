418fcee5a53b5fad877ab69c58eb045e10ca40b1
jann88 <jannylynemiaz@gmail.com>
2023-07-13 17:20:09 -0700

    returns a pointer to the allocated memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,19 @@
+#include <stdlib.h>
 #include "main.h"
 /**
  * malloc_checked - allocates memory using malloc
- * @b: input to checked
+ * @b: number of bytes to allocate
  * if malloc_checked fails, return 98
  * Return: pointer to the located memory
  */
 void *malloc_checked(unsigned int b)
 {
-	int* ptr = (int*) malloc(sizeof(int) * n);
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
 }
