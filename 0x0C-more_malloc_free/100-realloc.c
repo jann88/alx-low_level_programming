0ad92bcad43afeed2ccc71dad74539d92a61a3f2
jann88 <jannylynemiaz@gmail.com>
2023-07-14 10:36:24 -0700

    reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,6 +18,7 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *p;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -37,12 +38,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size > old_size)
 	{
-		ptr = malloc(new_size);
-		if (ptr == NULL)
+		p = malloc(new_size);
+		if (p == NULL)
 			return (NULL);
 		for (i = 0; i < old_size && i < new_size; i++)
 		{
-			*((char *)ptr + i);
+			*((char *)p + i) = *((char *)ptr + i);
 			free(ptr);
 		}
 	}
