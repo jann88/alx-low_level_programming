9a39222bdcb68b5f40ef927d8548af59e0e34813
jann88 <jannylynemiaz@gmail.com>
2023-07-19 16:45:34 -0700

    prints result, given integers and operators

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,14 +17,9 @@ int (*get_op_func(char *s))(int, int)
     };
     int i = 0;
 
-    while (i < 5)
+    while (ops[i].op != NULL && *(ops[i].op) != *s)
     {
 	    i++;
-	    if (s && s[0] == ops[i].op[0] && !s[1])
-	    {
-		    return (ops[i].f);
-	    }
-
     }
-    return (NULL);
+    return (ops[i].f);
 }
