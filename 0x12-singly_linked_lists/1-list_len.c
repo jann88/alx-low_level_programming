93025b6d0172404c6a60f930f3f4bb13de6b4a6f
jann88 <jannylynemiaz@gmail.com>
2023-07-27 15:22:08 -0700

    no of elements in h

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * list_len - prints the number of elements in a linked list
+ * @h: poiner to the list_t list
+ * Return: number of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	for (; i <= h; i++)
+	{
+		h->next;
+	}
+	return (i);
+}
