e6f8d046182b226b4fc39183953e5311805293d7
jann88 <jannylynemiaz@gmail.com>
2023-07-27 12:29:58 -0700

    prints all elements in a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_list - prints all elements in a list
+ * @list_t: given list
+ * @h: first node of the list element
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *h;
+	h = &h;
+	str = maloc((sizeof list_t));
+	if (str == NULL)
+	{
+		printf("[0] (nil)");
+	}
+	for (str = 0; str !='\0' && srt <= size_t; str++)
+		;
+	return (str);
+}
