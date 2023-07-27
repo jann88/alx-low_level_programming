29c88dc9240cfb985e2e61bd1ec16cf5db6bffe9
jann88 <jannylynemiaz@gmail.com>
2023-07-27 15:12:06 -0700

    prints linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,13 +12,12 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (!h->str)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
-	free (h);
 	return (i);
 }
