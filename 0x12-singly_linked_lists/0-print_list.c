6fdc4fc4a5dc7c0e0d4f32572d7953de44efc852
jann88 <jannylynemiaz@gmail.com>
2023-07-29 06:55:31 -0700

    declared a list integer

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 /**
  * print_list - prints a linked list
- * @list_t: given list
  * @h: first node of the list element
  * Return: number of nodes, size of t
  */
 size_t print_list(const list_t *h)
 {
-	list_t i = 0;
+	size_t i = 0;
 
 	while (h)
 	{
