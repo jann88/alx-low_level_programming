832a9f9bd3ba0c7602af7c1866a878b3a2d3b212
jann88 <jannylynemiaz@gmail.com>
2023-08-01 05:29:32 -0700

    deletes the first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t
+ * @head: a pointer to a pointer listint_t first node
+ * Return: 0 Always.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	temp = *head;
+	n = temp->n;
+	*head = (*head)->next;
+	free(temp);
+	return (n);
+}
