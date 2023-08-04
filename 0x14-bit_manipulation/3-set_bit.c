564d0cd569f252a7af32626d0ab79304ca4740d1
jann88 <jannylynemiaz@gmail.com>
2023-08-04 16:12:11 -0700

    edited documentation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
  * set_bit - sets the value of a bit to 1 at a given index
  * @n: pointer to the given bit code
  * @index: the bit to be set
- * Returns: 1 if it works and -1 if it fails
+ * Return: 1 if it works and -1 if it fails
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
