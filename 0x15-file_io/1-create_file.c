d3f97ee6c0fb21dbb8c19a151ba9f8cd615ed14a
jann88 <jannylynemiaz@gmail.com>
2023-08-07 08:33:52 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <string.h>
 /**
+ * _strlen - returns the length of a string
+ * @s: string to check
+ * Return: integer length of a string
+ */
+int _strlen(char *s)
+{
+        int i = 0;
+        if (!s)
+                return (0);
+        while (*s++)
+        {
+                i++;
+        }
+        return (i);
+}
+
+/**
  * create_file - creates a file with rw------- permissions
  * @filename: name of the file, if NULL, return -1
  * @text_content: contents of the file, if NULL , return -1i
@@ -10,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t bytes = 0;
 	int newfile;
-	ssize_t len = strlen(text_content);
+	ssize_t len = _strlen(text_content);
 
 	if (!filename)
 	{
