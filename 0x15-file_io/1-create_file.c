f255a0b4a99da51fdc1f1ebe6568541e591ef873
jann88 <jannylynemiaz@gmail.com>
2023-08-07 08:24:43 -0700

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,14 +20,9 @@ int create_file(const char *filename, char *text_content)
 	newfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (newfile == -1)
 		return (-1);
-	if (text_content == NULL)
-	{
-		close(newfile);
-		return (1);
-	}
 	if (len)
 		bytes = write(newfile, text_content, len);
 	close(newfile);
-	return (bytes == len? 1 : 1);
+	return (bytes == len ? 1 : 1);
 }
 
