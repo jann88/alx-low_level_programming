1f3c7848ffb8e085d016330a18c47ffa091571e5
jann88 <jannylynemiaz@gmail.com>
2023-08-07 14:52:58 -0700

    copies text to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+/**
+ * main - program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+	{
+		return (0);
+	}
+	while (*s++)
+	{
+		i++;
+	}
+	return (i);
+}
+int main(int argc, char *argv[])
+{
+	ssize_t bytes = 0;
+	int file_from;
+	int file_to;
+	/*int len;*/
+	char buf[BUFF_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, USAGE);
+		exit(97);
+	}
+	/*len = _strlen(argv[1]);*/
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
+		exit(99);
+	}
+	while ((bytes = read(file_from, buf, BUFF_SIZE)) > 0)
+	{
+		if (write(file_to, buf, bytes) != bytes)
+		{
+			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]);
+			exit(99);
+		}
+		if (bytes == -1)
+		{
+			dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]);
+			exit(98);
+		}
+		file_from = close(file_from);
+	}
+	if ((file_from) && (file_to))
+	{
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, file_from);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,15 +6,10 @@
 #include <fcntl.h>
 
 #define BUFF_SIZE 1024
-/* strlen func*/
-/**
- * _strlen - returns the length of a string
- * @s: string to check
- * Return: integer length of a string
- */
 int _strlen(char *s);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int copy_text_to_file(const char *filename, char *text_content);
 #endif
