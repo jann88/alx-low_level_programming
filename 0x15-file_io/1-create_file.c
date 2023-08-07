9a87828c6e68da2af37f97220ad3564cbbb4db33
jann88 <jannylynemiaz@gmail.com>
2023-08-07 08:15:22 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - creates a file with rw------- permissions
+ * @filename: name of the file, if NULL, return -1
+ * @text_content: contents of the file, if NULL , return -1i
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t bytes = 0;
+	int newfile;
+	ssize_t len = strlen(text_content);
+
+	if (!filename)
+	{
+		return (-1);
+
+	}
+	newfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (!newfile)
+		return (-1);
+	if (len)
+		bytes = write(newfile, text_content, len);
+	close(newfile);
+	return (bytes);
+}
+
