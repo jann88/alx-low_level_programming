7146b27576b67cd869001f1014143e5a0e8a4295
jann88 <jannylynemiaz@gmail.com>
2023-08-07 15:54:22 -0700

    copies text to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -53,9 +53,13 @@ int main(int argc, char *argv[])
 	}
 	file_from = close(file_from);
 	file_to = close(file_to);
-	if ((file_from == -1) || (file_to == -1))
+	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, ((file_from) || (file_to)), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, file_from), exit(100);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, file_to) ,exit(100);
 	}
 	return (EXIT_SUCCESS);
 }
