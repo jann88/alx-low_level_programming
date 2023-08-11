8aa3d33e0a820bfd27e2299e5324a7c01377bbca
jann88 <jannylynemiaz@gmail.com>
2023-08-11 16:15:09 -0700

    task 3 error fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
 #define USAGE "Usage: cp file_from file_to\n"
 #define ERR_NOREAD "Error: Can't read from file %s\n"
