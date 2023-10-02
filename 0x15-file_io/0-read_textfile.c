ea868ca49e5e2f482da31bac210014578f3f8b13
jann88 <jannylynemiaz@gmail.com>
2023-10-02 15:57:32 -0700

    function that reads and prints standard output to the POSIX

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,26 +12,22 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-
 	ssize_t _read, _write;
-
 	char *buffer;
 
-        if (filename == NULL)
-        {
-                return (0);
-        }
-	/*open file */
-        fd = open(filename, O_RDONLY);
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	/*open */
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-       /*buffer */
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-	/* read */
 	_read = read(fd, buffer, letters);
 	if (_read == -1)
 	{
@@ -39,7 +35,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	/*write */
 	_write = write(STDOUT_FILENO, buffer, _read);
 	if (_write == -1)
 	{
@@ -49,6 +44,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	close(fd);
 	return (_read);
-		
-        
-}  
+}
