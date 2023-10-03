13e4a17c2e88ab2156df36e63ab263ca151171db
jann88 <jannylynemiaz@gmail.com>
2023-10-02 17:10:06 -0700

    copies from one file to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,12 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
+#define BUFFER_SIZE 1024
+int _putchar(char c);
+void err_98(int fd_from, char *buffer, char *argv);
+void err_99(int fd_from, char *buffer, char *argv);
+void err_100(int fd_from, char *buffer);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
