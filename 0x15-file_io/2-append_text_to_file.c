47d22f4268df31dc5ec20a57f78d266b9b4541f6
jann88 <jannylynemiaz@gmail.com>
2023-10-02 17:02:30 -0700

    appends to a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to the end of the file
+ * @filename: file to append
+ * text_content: NULL terminated string to add to the file
+ * Return: 1 success, -1 failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, _write;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	_write = write(fd, text_content, strlen(text_content));
+	if (_write == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return(1);
+}
