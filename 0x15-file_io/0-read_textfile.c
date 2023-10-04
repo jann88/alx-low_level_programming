3faf8ec9a667491e19bfe387f1aa78ee9e2281b8
jann88 <jannylynemiaz@gmail.com>
2023-10-04 16:01:34 -0700

    modified if statement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,35 +11,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t _read, _write;
-	char *buffer;
+	ssize_t fd, _read, _write;
+	char *buffer = malloc(sizeof(char) * letters);
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-	/*open */
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		return (0);
-	}
-	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
+	fd = open(filename,O_RDONLY);
 	_read = read(fd, buffer, letters);
-	if (_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
 	_write = write(STDOUT_FILENO, buffer, _read);
-	if (_write == -1 || _write != _read)
+	if (fd == -1 || _read == -1 || _write == -1 || _write != _read)
 	{
 		free(buffer);
-		close(fd);
 		return (0);
 	}
 	free(buffer);
