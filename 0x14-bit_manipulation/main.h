042189cee12ecf3c3ab939ce85ca318e517f6b42
jann88 <jannylynemiaz@gmail.com>
2023-10-04 12:57:20 -0700

    added bit manipulation notes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
